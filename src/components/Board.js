7127ad821777f8eb03d273ac5d7960473d6d752e
Kerem Dilmen <kerem.dilmen@berkeley.edu>
2022-03-15 16:19:43 -0700

    finished hw4

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/Board.css';
 import Square from "./Square";
+import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
 
 function Board() {
 
-    const status = 'Next player: X';
+    var status = "Next player: X";
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setNext] = useState(true);
 
-    function renderSquare(i) {
-        return <Square />;
+    function testWinner(squares) {
+      const posn = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ]
+
+      for (let i = 0; i < posn.length; i++) {
+        const [x, y, z] = posn[i];
+        if (squares[x] && squares[x] === squares[y] && squares[x] === squares[z]) {
+          return squares[x];
+        }
+      }
+
+      return null;
     }
 
-    return (  
+    function handleClick(i) {
+      const curr_squares = squares.slice();
+      if (testWinner(curr_squares) || curr_squares[i]) {
+        return;
+      }
+      
+      curr_squares[i] = xIsNext ? 'X': 'O';
+      setSquares(curr_squares);
+      xIsNext = setNext(!xIsNext);
+    }
+
+    function renderSquare(i) {
+      return (
+        <Square 
+          value = {squares[i]}
+          onClick = {() => handleClick(i)}
+      />);
+    }
+
+    
+    const winner = testWinner(squares);
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (xIsNext ? 'X': 'O');
+    }
+    
+
+    return ( 
         <div>
           <div className="board-row">
             {renderSquare(0)}
